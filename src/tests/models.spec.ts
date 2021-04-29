cbb831b72dd519caa30d16867dc7078e2cf5e06b
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-04-28 23:35:31 -0300

    feat: role model

--- src/tests/models.spec.ts
+++ src/tests/models.spec.ts
@@ -1,4 +1,5 @@
 import { Permission } from '@models/Permission'
+import { Role } from '@models/Role'
 import { createConnection, getConnectionOptions, getRepository } from 'typeorm'
 
 async function getConn () {
@@ -71,6 +72,74 @@ describe('Permission Model', () => {
     })
 
     const result = await permissionRepository.delete(permission)
+
+    expect(result).toHaveProperty('affected', 1)
+  })
+})
+
+describe('Role Model', () => {
+  it('Should be able to insert a role', async () => {
+    const roleRepository = getRepository(Role)
+
+    const insertedRole = await roleRepository.insert({
+      name: 'CLIENT'
+    })
+
+    expect(insertedRole.raw).toHaveLength(1)
+  })
+
+  it('Should be able to find created role', async () => {
+    const roleRepository = getRepository(Role)
+
+    const role = await roleRepository.findOne({
+      where: {
+        name: 'CLIENT'
+      }
+    })
+
+    expect(role).toHaveProperty('name', 'CLIENT')
+  })
+
+  it('Should not be able to create a role with the same name', async () => {
+    const roleRepository = getRepository(Role)
+
+    try {
+      const duplicatedRole = await roleRepository.insert({
+        name: 'CLIENT'
+      })
+
+      expect(duplicatedRole.raw).toHaveLength(0)
+    } catch (err) {
+      expect(err).toHaveProperty('code', '23505') // duplicate key value error
+    }
+  })
+
+  it('Should be able to edit role name', async () => {
+    const roleRepository = getRepository(Role)
+
+    const role = await roleRepository.findOne({
+      where: {
+        name: 'CLIENT'
+      }
+    })
+
+    const result = await roleRepository.update(role, {
+      name: 'EMPLOYEE'
+    })
+
+    expect(result).toHaveProperty('affected', 1)
+  })
+
+  it('Should be able to delete role', async () => {
+    const roleRepository = getRepository(Role)
+
+    const role = await roleRepository.findOne({
+      where: {
+        name: 'EMPLOYEE'
+      }
+    })
+
+    const result = await roleRepository.delete(role)
 
     expect(result).toHaveProperty('affected', 1)
   })
