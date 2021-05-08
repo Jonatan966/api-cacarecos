f2bc8e6c88a4ed9d355e4e09cb86ecddcfe7f7eb
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-07 22:50:54 -0300

    feat: error object interface

--- src/hooks/useErrorMessage.ts
+++ src/hooks/useErrorMessage.ts
@@ -1,5 +1,13 @@
 import { Response } from 'express'
 
+export interface ErrorObject {
+  error: {
+    message: string;
+    statusCode: number;
+    [key: string]: any;
+  }
+}
+
 export function useErrorMessage (message: string, statusCode: number, response: Response, extra?: any) {
   return response
     .status(statusCode)
