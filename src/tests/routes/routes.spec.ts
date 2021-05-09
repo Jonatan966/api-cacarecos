b4da3b570ef50e752275c2d8e9931d2318ec19c6
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-09 01:29:54 -0300

    feat: product routes tests

--- src/tests/routes/routes.spec.ts
+++ src/tests/routes/routes.spec.ts
@@ -4,6 +4,7 @@ import { app } from '../../app'
 import { connectToDatabase } from '../setup'
 import { categoryRoutesTests } from './category'
 import { permissionRoutesTests } from './permission'
+import { productRouteTests } from './product'
 import { roleRoutesTests } from './role'
 
 const appRoutes = supertest(app)
@@ -13,3 +14,4 @@ beforeAll(connectToDatabase)
 permissionRoutesTests(appRoutes)
 roleRoutesTests(appRoutes)
 categoryRoutesTests(appRoutes)
+productRouteTests(appRoutes)
