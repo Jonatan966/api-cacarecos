5f5038c378070f1e105ca348150c659c132c4d34
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-12 19:38:26 -0300

    feat: auth controller routes

--- src/app/routes/public.routes.ts
+++ src/app/routes/public.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { AuthController } from '@controllers/AuthController'
 import { CategoryController } from '@controllers/CategoryController'
 import { ProductController } from '@controllers/ProductController'
 import { UserController } from '@controllers/UserController'
@@ -13,5 +14,8 @@ publicRoutes.get('/products', ProductController.index)
 publicRoutes.get('/products/:id', ProductController.show)
 
 publicRoutes.post('/users', UserController.create)
+
+publicRoutes.post('/auth/login', AuthController.logIn)
+publicRoutes.get('/auth/logout', AuthController.validate, AuthController.logOut)
 
 export { publicRoutes }
