58bbc712ae58c1f0a37a18d30f4f08863e2a8537
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-12 19:37:54 -0300

    feat: controller types improvement

--- src/@types/Controller.ts
+++ src/@types/Controller.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express'
 
 import { ErrorObject } from '@hooks/useErrorMessage'
+import { User } from '@models/User'
 
-export interface Controller<T = any> {
-  create: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
+export interface NewResponse<T = any> extends Response<T> {
+  locals: {
+    user: Pick<User, 'id' | 'roles'>
+  }
+}
+
+export interface DefaultController<T = any> {
+  create: (req: Request, res: NewResponse<T>) => Promise<NewResponse<T | ErrorObject>>;
   remove: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
   index: (req: Request, res: Response<T[]>) => Promise<Response<T[] | ErrorObject>>;
   show: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
   update?: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
+}
+
+export interface TemplateController {
+  [name: string]: (req: Request, res: NewResponse) => Promise<any>;
 }
