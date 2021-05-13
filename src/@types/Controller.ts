5a3e2619ab9a42e1197114f00f82bfb8495cc926
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-13 14:14:43 -0300

    fix: make show function optional

--- src/@types/Controller.ts
+++ src/@types/Controller.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from 'express'
 
 import { ErrorObject } from '@hooks/useErrorMessage'
+import { Product } from '@models/Product'
 import { User } from '@models/User'
 
 export interface NewResponse<T = any> extends Response<T> {
   locals: {
     user: Pick<User, 'id' | 'roles'>
+    product: Product
   }
 }
 
 export interface DefaultController<T = any> {
   create: (req: Request, res: NewResponse<T>) => Promise<NewResponse<T | ErrorObject>>;
-  remove: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
-  index: (req: Request, res: Response<T[]>) => Promise<Response<T[] | ErrorObject>>;
-  show: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
-  update?: (req: Request, res: Response<T>) => Promise<Response<T | ErrorObject>>;
+  remove: (req: Request, res: NewResponse<T>) => Promise<NewResponse<T | ErrorObject>>;
+  index: (req: Request, res: NewResponse<T[]>) => Promise<NewResponse<T[] | ErrorObject>>;
+  show?: (req: Request, res: NewResponse<T>) => Promise<NewResponse<T | ErrorObject>>;
+  update?: (req: Request, res: NewResponse<T>) => Promise<NewResponse<T | ErrorObject>>;
 }
 
 export interface TemplateController {
