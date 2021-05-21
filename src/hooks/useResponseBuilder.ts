ab60d6515f19389ee68793e023694a3b74ad96ef
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-21 17:43:44 -0300

    feat: response builder hook

---
+++ src/hooks/useResponseBuilder.ts
@@ -0,0 +1,23 @@
+import { Repository } from 'typeorm'
+
+import { AppPaginatorProps } from '@interfaces/Paginator'
+
+export async function useResponseBuilder<S> (
+  searchResult: S[],
+  paginator: AppPaginatorProps,
+  searchParams: Partial<S>,
+  repository: Repository<S>
+) {
+  const page = (paginator.skip / paginator.take) + 1
+
+  const dataCount = await repository.count({
+    where: searchParams
+  })
+
+  return {
+    page,
+    previous_page: (page > 1) && `/_page=${page - 1}&_max=${paginator.take}`,
+    next_page: ((paginator.skip + paginator.take) < dataCount) && `/_page=${page + 1}&_max=${paginator.take}`,
+    results: searchResult
+  }
+}
