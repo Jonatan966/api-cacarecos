689101c3ac22896d5055cf3f13180916d6945a01
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-26 18:06:37 -0300

    feat: add stock permissions

--- src/tests/userAdminCreation.ts
+++ src/tests/userAdminCreation.ts
@@ -27,12 +27,13 @@ export const userAdminCreation = () => {
        ,('EDIT_USER'),('ADD_PERMISSION'),('LIST_PERMISSIONS')
        ,('REMOVE_PERMISSION'),('ADD_ROLE'),('EDIT_ROLE')
        ,('REMOVE_ROLE'),('VIEW_ROLES'),('UPDATE_ORDER'),('UPDATE_USER')
+       ,('ADD_STOCK'),('LIST_STOCKS')
       `)
 
       const createdPermissions = await getManager().query('SELECT COUNT(*) FROM permissions')
 
       expect(createdPermissions).toHaveLength(1)
-      expect(createdPermissions[0]).toHaveProperty('count', '24')
+      expect(createdPermissions[0]).toHaveProperty('count', '26')
     })
 
     it('Should be able to create roles', async () => {
