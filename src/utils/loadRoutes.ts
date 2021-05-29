0a16db3d9c61f47ffd3de819ca7d93ac8247285a
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-05-29 14:59:05 -0300

    feat: add globalMiddlewares in route list

--- src/utils/loadRoutes.ts
+++ src/utils/loadRoutes.ts
@@ -6,11 +6,21 @@ export function loadRoutes (routesObject: RouteList, expressRouter: Router) {
   const routePaths = Object.keys(routesObject)
 
   for (const routePath of routePaths) {
-    const routeMethods = Object.keys(routesObject[routePath])
+    let routeMethods = Object.keys(routesObject[routePath])
+
+    const globalMiddlewares = routeMethods
+      .includes('globalMiddlewares')
+      ? routesObject[routePath].globalMiddlewares
+      : []
+
+    routeMethods = routeMethods.filter(routeMethod =>
+      routeMethod !== 'globalMiddlewares'
+    )
 
     routeMethods.forEach(routeMethod => {
       expressRouter[routeMethod](
         routePath,
+        globalMiddlewares,
         routesObject[routePath][routeMethod]
       )
     })
