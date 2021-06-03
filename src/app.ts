9bb3df88e86103e41754ea5914b7a6c88c657a76
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-06-03 13:50:37 -0300

    refact: remove cors policy

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,7 @@ import { routes } from './app/routes'
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  credentials: true,
-  origin: process.env.WEBSITE_URL
-}))
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
