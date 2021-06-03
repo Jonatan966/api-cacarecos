37ef48b7552a0cddde10791e7bd2ea5c87d49ae2
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-06-03 02:00:16 -0300

    feat: update cors policy

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import { routes } from './app/routes'
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  credentials: true,
+  origin: process.env.WEBSITE_URL
+}))
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
